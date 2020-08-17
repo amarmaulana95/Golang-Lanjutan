1c400280b9d36d1028a46de08be06486505af650
Amar <42346300+amarmaulana95@users.noreply.github.com>
2020-08-18 00:35:36 +0700

    Update koneksi.go

--- koneksi/koneksi.go
+++ koneksi/koneksi.go
@@ -4,7 +4,7 @@ import "database/sql"
 import _ "mysql-master"
 
 func Connect() (*sql.DB, error){
-  db, err := sql.Open("mysql", "root:AmikBsi12119617@tcp(127.0.0.1:3306)/post")
+  db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/post")
   if err != nil {
     return nil, err
   }
